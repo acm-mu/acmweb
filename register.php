17466fd745191a31415aee1b9bb8cfd39ad4eb60
Jack Pfeiffer <jl5her@yahoo.com>
2019-12-26 22:06:34 -0600

    Changes to register page

--- register.php
+++ register.php
@@ -6,15 +6,21 @@ Link with timestamp in url to prevent browser from caching. -->
 
 <h1>Register</h1>
 
-<h3>School Information</h3>
-<label>Email Address</label>
-<input placeholder="Ferris.Bueller@example.com" type="email">
-
+<!-- <h3>School Profile</h3>
 <label>School Name</label>
 <input placeholder="Glenbrook High School">
 
 <label>Coach Name</label>
 <input placeholder="Ferris Bueller">
+
+<label>Email Address</label>
+<input placeholder="Ferris.Bueller@example.com" type="email">
+
+<label>Password</label>
+<input type='password'>
+
+<label>Confirm Password</label>
+<input type='password'>
 
 <label>Coach Telephone</label>
 <input placeholder="(000) 000-0000">
@@ -26,16 +32,117 @@ Link with timestamp in url to prevent browser from caching. -->
     <option>L</option>
     <option>XL</option>
     <option>XXL</option>
-</select>
+</select> -->
 
 <h3>Blue Division</h3>
 <label>Are you bringing teams for the Blue (Java/Python) Division?</label>
 
+<label class='switch'>
+    <input type='checkbox' id='blue'>
+    <span class='slider'></span>
+</label>
+
+<table id='blue'>
+    <tr class="prototype">
+        <td><button class='del'>-</button></td>
+        <td> <input placeholder='First Name'></td>
+        <td><input placeholder='Last Name'> </td>
+        <td>
+            <select class='custom-select'>
+                <option style='color: #ddd !important'>Shirt Size</option>
+                <option>Small</option>
+                <option>Medium</option>
+                <option>Large</option>
+                <option>X-Large</option>
+                <option>XX-Large</option>
+            </select>
+        </td>
+    </tr>
+    <tr>
+        <td><button class='add'>+</button></td>
+    </tr>
+</table>
+
 <h3>Gold Division</h3>
 <label>Are you bringing teams for the Gold (Scratch) Division?</label>
+
+<label class='switch'>
+    <input type='checkbox' id='gold'>
+    <span class='slider'></span>
+</label>
+
+<table id='gold'>
+    <tr class="prototype">
+        <td><button class='del'>-</button></td>
+        <td> <input placeholder='First Name'></td>
+        <td><input placeholder='Last Name'> </td>
+        <td>
+            <select class='custom-select'>
+                <option style='color: #ddd !important'>Shirt Size</option>
+                <option>Small</option>
+                <option>Medium</option>
+                <option>Large</option>
+                <option>X-Large</option>
+                <option>XX-Large</option>
+            </select>
+        </td>
+    </tr>
+    <tr>
+        <td><button class='add'>+</button></td>
+    </tr>
+</table>
 
 <h3>Eagle Division</h3>
 <label>Are you bringing teams for the Eagle Division?</label>
 
+<label class='switch'>
+    <input type='checkbox' id='eagle'>
+    <span class='slider'></span>
+</label>
+
+<table id='eagle'>
+    <tr class="prototype">
+        <td><button class='del'>-</button></td>
+        <td> <input placeholder='First Name'></td>
+        <td><input placeholder='Last Name'> </td>
+        <td>
+            <select class='custom-select'>
+                <option style='color: #ddd !important'>Shirt Size</option>
+                <option>Small</option>
+                <option>Medium</option>
+                <option>Large</option>
+                <option>X-Large</option>
+                <option>XX-Large</option>
+            </select>
+        </td>
+    </tr>
+    <tr>
+        <td><button class='add'>+</button></td>
+    </tr>
+</table>
+
 </form>
 <?php require_once("footer.php"); ?>
+
+<script>
+$(document).ready(function() {
+    $(document).on('click', '.add', function() {
+        var table = $(this).closest('table')
+        var tr = table.find('tr.prototype')
+
+        var newTr = tr.clone()
+        newTr.removeClass("prototype")
+        newTr.insertBefore($('.add').closest('tr'))
+    })
+
+    $(document).on('click', "input[type='checkbox']", function() {
+        var id = $(this).attr('id')
+        var table = $('table#' + id)
+        table.css('display', $(this).prop('checked') ? 'block' : 'none')
+    })
+
+    $(document).on('click', '.del', function() {
+        $(this).closest('tr').remove()
+    })
+})
+</script>
