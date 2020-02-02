bf60636c8a0dc7528e49305489f8cc98da0152fe
Jack Pfeiffer <jl5her@yahoo.com>
2020-02-01 22:16:46 -0600

    added more info for registration

--- competition.php
+++ competition.php
@@ -25,7 +25,7 @@ Link with timestamp in url to prevent browser from caching. -->
 
     This Competition features three divisions:
 
-    <br /> 
+    <br />
 </p>
 
 <h2>Java/Python Division (Blue)</h2>
@@ -34,7 +34,10 @@ Link with timestamp in url to prevent browser from caching. -->
     Contest. Teams of three or four students will have three hours and two computers to work collaboratively to solve
     problems similar in scope to Advanced Placement Computer Science exam questions. Points will be awarded based on the
     number of problems correctly solved and the time taken to solve, with appropriate penalties for incorrect
-    submissions. Development environments available will most likely include <a href="https://www.eclipse.org/">Eclipse</a>, <a href="https://netbeans.org/">NetBeans</a>, WordPad + JDK, <a href="https://www.cygwin.com/">Cygwin tools</a>, <a href="https://www.bluej.org/">BlueJ</a>, and <a href="https://www.jetbrains.com/idea/">IntelliJ</a>.
+    submissions. Development environments available will most likely include <a
+        href="https://www.eclipse.org/">Eclipse</a>, <a href="https://netbeans.org/">NetBeans</a>, WordPad + JDK, <a
+        href="https://www.cygwin.com/">Cygwin tools</a>, <a href="https://www.bluej.org/">BlueJ</a>, and <a
+        href="https://www.jetbrains.com/idea/">IntelliJ</a>.
 
     <br />
     <p>
@@ -45,7 +48,9 @@ Link with timestamp in url to prevent browser from caching. -->
 <h2>Open/Scratch Division (Gold)</h2>
 <p>
     Teams of two or three students will have three hours and one computer to implement a themed <a
-        href='http://scratch.mit.edu/'>Scratch</a> project. Submissions will be judged on creativity, use of key Scratch constructs, and theme correspondence. This division will be suitable for introductory students familiar with Scratch.
+        href='http://scratch.mit.edu/'>Scratch</a> project. Submissions will be judged on creativity, use of key Scratch
+    constructs, and theme correspondence. This division will be suitable for introductory students familiar with
+    Scratch.
 </p>
 <!-- <p>You may find it useful to review our Scratch Preparation Notes.</p> -->
 <h2>AP Computer Science Principles (Eagle)</h2>
@@ -66,7 +71,8 @@ Link with timestamp in url to prevent browser from caching. -->
 </p>
 
 <h3>Schedule:</h3>
-<p><em>This is a tenative schedule and could change at any time. We will try our best to notify you of any changes.</em></p>
+<p><em>This is a tenative schedule and could change at any time. We will try our best to notify you of any changes.</em>
+</p>
 <ul>
     <li>Registration/Welcome: <b>8:00 AM - 8:30 AM</b></li>
     <li>Practice Problem: <b>8:30 AM - 9:00 AM</b></li>
@@ -81,7 +87,9 @@ Link with timestamp in url to prevent browser from caching. -->
 
 <!-- <h3>Register</h3> -->
 <a href="/register">
-        <button class="register">Register</button>
-    </a>
-
+    <button class="register">Register</button>
+</a>
+<p>
+    <i>Registration closes March 27th, 2020</i>
+</p>
 <?php require_once $_SERVER['DOCUMENT_ROOT'] . "/include/footer.php"; ?>
