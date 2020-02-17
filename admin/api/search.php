3ec1098008e0a5ee20f1b2d146719f1e19a78092
Jack Pfeiffer <jl5her@yahoo.com>
2020-02-17 17:43:32 -0600

    add search functionality

--- admin/api/search.php
+++ admin/api/search.php
@@ -7,16 +7,13 @@ if (!loggedin()) {
 }
 
 $search = "";
-if (isset($_GET['search']))
+if (isset($_GET['search'])) {
     $search = $_GET['search'];
+}
 
-$schoolid = "";
-if (isset($_GET['schoolid']))
-    $schoolid = "school.schoolid=".$_GET['schoolid']." AND";
-
-$sql = "SELECT * FROM"
-. " team INNER JOIN school ON team.schoolid = school.schoolid"
-. " WHERE $schoolid (school.sname LIKE '%$search%' OR team.tname LIKE '%$search%' OR school.scity LIKE '%$search%')"
+$sql = "SELECT * FROM" 
+. " team INNER JOIN school ON team.schoolid = school.schoolid" 
+. " WHERE (school.sname LIKE '%$search%' OR team.tname LIKE '%$search%' OR school.scity LIKE '%$search%')"
 . " ORDER BY rdate DESC";
 
 $res = $mysql->query($sql);
@@ -25,4 +22,5 @@ while($row = $res->fetch_assoc()) {
     $rows[] = $row;
 }
 echo json_encode($rows);
+
 ?>
