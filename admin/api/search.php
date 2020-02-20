7ae7757a1c919e152a673a77871350a1bb9d6d2d
Jack Pfeiffer <jl5her@yahoo.com>
2020-02-20 13:25:41 -0600

    update wrong member count

--- admin/api/search.php
+++ admin/api/search.php
@@ -11,7 +11,7 @@ if (isset($_GET['search'])) {
     $search = $_GET['search'];
 }
 
-$sql = "SELECT * FROM" 
+$sql = "SELECT team.*, school.*, coach.cname FROM" 
 . " team INNER JOIN school ON team.schoolid = school.schoolid" 
 . " INNER JOIN coach ON coach.schoolid = school.schoolid"
 . " WHERE (school.sname LIKE '%$search%' OR team.tname LIKE '%$search%' OR school.scity LIKE '%$search%' OR coach.cname LIKE '%$search%')"
