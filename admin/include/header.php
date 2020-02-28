9b5d3f3fc9c51a862ce9a88e2432c4357fe29fe2
Jack Pfeiffer <jl5her@yahoo.com>
2020-02-28 00:01:28 -0600

    Update statistics to account for overflow

--- admin/include/header.php
+++ admin/include/header.php
@@ -16,85 +16,112 @@ if (!loggedin()) {
     exit();
 }
 ?>
-<div class="ui grid">
-    <div class="three wide column">
-        <div class="ui horizontal tiny statistics">
-            <a href='/admin/schools'>
-                <div class="statistic">
-                    <div class="value" id="schools">0</div>
-                    <div class="label">Schools</div>
-                </div>
-            </a><a>
-                <div class="statistic">
-                    <div class="value" id="coaches">0</div>
-                    <div class="label">Coaches</div>
-                </div>
-            </a>
-        </div>
-    </div>
-    <div class="three wide column">
-        <div class="ui horizontal tiny statistics">
-            <a href='/admin/teams'>
-                <div class="statistic">
-                    <div class="value" id="teams">0</div>
-                    <div class="label">Teams</div>
-                </div>
-            </a>
-            <a>
-                <div class="statistic">
-                    <div class="value" id="students">0</div>
-                    <div class="label">Students</div>
-                </div>
-            </a>
-        </div>
-    </div>
-    <div class="three wide column">
-        <div class="ui horizontal tiny statistics">
-            <a href='/admin/teams?division=blue'>
-                <div class="blue statistic">
-                    <div class="value" id="blue_teams">0</div>
-                    <div class="label">Teams</div>
-                </div>
-            </a>
-            <a>
-                <div class="blue statistic">
-                    <div class="value" id="blue_students">0</div>
-                    <div class="label">Students</div>
-                </div>
-            </a>
-        </div>
-    </div>
-    <div class="three wide column">
-        <div class="ui horizontal tiny statistics">
-            <a href='/admin/teams?division=gold'>
-                <div class="yellow statistic">
-                    <div class="value" id="gold_teams">0</div>
-                    <div class="label">Teams</div>
-                </div>
-            </a>
-            <a>
-                <div class="yellow statistic">
-                    <div class="value" id="gold_students">0</div>
-                    <div class="label">Students</div>
-                </div>
-            </a>
-        </div>
-    </div>
 
-    <div class="three wide column">
-        <div class="ui horizontal tiny statistics">
-            <a href='/admin/teams?division=eagle'>
-                <div class="teal statistic">
-                    <div class="value" id="eagle_teams">0</div>
-                    <div class="label">Teams</div>
-                </div>
-            </a>
-            <a>
-                <div class="teal statistic">
-                    <div class="value" id="eagle_students">0</div>
-                    <div class="label">Students</div>
-                </div>
-            </a>
+<div class="ui grid" style="margin: 2px 0">
+    <div class="row">
+        <div class="three wide column"></div>
+        <div class="two wide column">
+            <div class="ui tiny statistics">
+                <a href='/admin/schools'>
+                    <div class="statistic">
+                        <div class="value" id="schools">0</div>
+                        <div class="label">Schools</div>
+                    </div>
+                </a>
+            </div>
+        </div>
+        <div class="two wide column">
+            <div class="ui tiny statistics">
+                <a href='/admin/teams'>
+                    <div class="statistic">
+                        <div class="value" id="teams">0</div>
+                        <div class="label">Teams</div>
+                    </div>
+                </a>
+            </div>
+        </div>
+        <div class="two wide column">
+            <div class="ui tiny statistics">
+                <a href='/admin/teams?division=blue'>
+                    <div class="blue statistic">
+                        <div class="value" id="blue_teams">0</div>
+                        <div class="label">Teams</div>
+                    </div>
+                </a>
+            </div>
+        </div>
+        <div class="two wide column">
+            <div class="ui tiny statistics">
+                <a href='/admin/teams?division=gold'>
+                    <div class="yellow statistic">
+                        <div class="value" id="gold_teams">0</div>
+                        <div class="label">Teams</div>
+                    </div>
+                </a>
+            </div>
+        </div>
+        <div class="two wide column">
+            <div class="ui tiny statistics">
+                <a href='/admin/teams?division=eagle'>
+                    <div class="teal statistic">
+                        <div class="value" id="eagle_teams">0</div>
+                        <div class="label">Teams</div>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="three wide column"></div>
+        <div class="two wide column">
+            <div class="ui tiny statistics">
+                <a>
+                    <div class="statistic">
+                        <div class="value" id="coaches">0</div>
+                        <div class="label">Coaches</div>
+                    </div>
+                </a>
+            </div>
+        </div>
+        <div class="two wide column">
+            <div class="ui tiny statistics">
+                <a>
+                    <div class="statistic">
+                        <div class="value" id="students">0</div>
+                        <div class="label">Students</div>
+                    </div>
+                </a>
+            </div>
+        </div>
+        <div class="two wide column">
+            <div class="ui tiny statistics">
+                <a>
+                    <div class="blue statistic">
+                        <div class="value" id="blue_students">0</div>
+                        <div class="label">Students</div>
+                    </div>
+                </a>
+            </div>
+        </div>
+        <div class="two wide column">
+            <div class="ui tiny statistics">
+                <a>
+                    <div class="yellow statistic">
+                        <div class="value" id="gold_students">0</div>
+                        <div class="label">Students</div>
+                    </div>
+                </a>
+            </div>
+        </div>
+        <div class="two wide column">
+            <div class="ui tiny statistics">
+                <a>
+                    <div class="teal statistic">
+                        <div class="value" id="eagle_students">0</div>
+                        <div class="label">Students</div>
+                    </div>
+                </a>
+            </div>
         </div>
     </div>
 </div>
@@ -128,77 +155,61 @@ if (!loggedin()) {
 </div>
 
 <script>
-function statistic(label, html, color) {
-    return $("<div/>").addClass("ui small statistic " + color).append(
-        $("<div/>", {
-            class: "label",
-            html: label
-        }),
-        $("<div/>", {
-            class: "value",
-            html: html
+    function fillStatistics(data) {
+        var nstudents = {
+            'total': 0,
+            'blue': 0,
+            'gold': 0,
+            'eagle': 0
+        }
+        var teams = {
+            'total': 0,
+            'blue': 0,
+            'gold': 0,
+            'eagle': 0
+        }
+        var coaches = 0
+
+        for (const school of data) {
+            coaches += school.small + school.medium + school.large + school.xlarge + school.xxlarge
+            for (const team of school.teams) {
+                const total = team.small + team.medium + team.large + team.xlarge + team.xxlarge
+
+                nstudents[team.division] += total
+                nstudents['total'] += total
+
+                teams['total']++
+                teams[team.division]++
+            }
+        }
+
+        $("#schools").html(data.length)
+        $("#coaches").html(coaches)
+
+        $("#students").html(nstudents['total'])
+        $("#blue_students").html(nstudents['blue'])
+        $("#gold_students").html(nstudents['gold'])
+        $("#eagle_students").html(nstudents['eagle'])
+
+        $("#teams").html(teams['total'])
+        $("#blue_teams").html(teams['blue'])
+        $("#gold_teams").html(teams['gold'])
+        $("#eagle_teams").html(teams['eagle'])
+    }
+
+    $(document).ready(function () {
+        $.ajax("/admin/api/schools", {
+            success: function (data) {
+                var jsonData = JSON.parse(data)
+                if (jsonData == 0) return
+                fillStatistics(jsonData)
+            }
         })
-    )
-}
-
-function fillStatistics(data) {
-    var nstudents = {
-        'total': 0,
-        'blue': 0,
-        'gold': 0,
-        'eagle': 0
-    }
-    var teams = {
-        'total': 0,
-        'blue': 0,
-        'gold': 0,
-        'eagle': 0
-    }
-    var coaches = 0
-
-    for (const school of data) {
-        coaches += school.small + school.medium + school.large + school.xlarge + school.xxlarge
-        for (const team of school.teams) {
-            const total = team.small + team.medium + team.large + team.xlarge + team.xxlarge
-
-            nstudents[team.division] += total
-            nstudents['total'] += total
-
-            teams['total']++
-            teams[team.division]++
-        }
-    }
-
-    $("#schools").html(data.length)
-    $("#coaches").html(coaches)
-
-    $("#students").html(nstudents['total'])
-    $("#blue_students").html(nstudents['blue'])
-    $("#gold_students").html(nstudents['gold'])
-    $("#eagle_students").html(nstudents['eagle'])
-
-    $("#teams").html(teams['total'])
-    $("#blue_teams").html(teams['blue'])
-    $("#gold_teams").html(teams['gold'])
-    $("#eagle_teams").html(teams['eagle'])
-}
-
-$(document).ready(function() {
-    $.ajax("/admin/api/schools", {
-        success: function(data) {
-            var jsonData = JSON.parse(data)
-            if (jsonData == 0) return
-            fillStatistics(jsonData)
-        },
-        error: function() {
-
-        }
     })
-})
 </script>
 
 <style>
-.ui.horizontal.statistics>.statistic {
-    margin: .5em;
-}
+    .two.wide.column .ui.statistics a {
+        margin: 0 auto;
+    }
 </style>
