fdf43060ccc78eb603fbc334d45744523127a21e
Jack Pfeiffer <jl5her@yahoo.com>
2020-03-10 22:36:08 -0400

    Invoice page

---
+++ admin/invoices.php
@@ -0,0 +1,149 @@
+<?php
+require_once $_SERVER['DOCUMENT_ROOT'] . "/admin/include/header.php";
+?>
+
+<table id="schools" class="ui very basic table">
+    <thead>
+        <tr>
+            <th col='sname' class='three wide'>School Name</th>
+            <th col='amountdue' class='two wide'>Amount Due</th>
+            <th col='view' class='two wide'>View Invoice</th>
+            <th col='sent' col='two wide'>Invoice Sent?</th>
+            <th col='paid' col='two wide'>Invoice Paid?</th>
+            <th col='rdate' class='three wide'>Registration Date</th>
+        </tr>
+    </thead>
+    <tbody> </tbody>
+</table>
+
+<script>
+$(document).ready(function() {
+    updateTeams()
+
+    $(document).on('click', '.sentInvoice', sentInvoice)
+    $(document).on('click', '.paidInvoice', paidInvoice)
+})
+
+function sentInvoice() {
+    $(this).addClass("loading")
+    const schoolid = $(this).attr('schoolid')
+    console.log("Sent invoice for " + schoolid)
+    $.ajax(`/admin/php/update?schoolid=${schoolid}&key=datesent`, {
+        success: updateTeams, 
+        error: function() {
+            console.error("An error has occurred updating the school's invoice (232)");
+        }
+    })
+}
+
+function paidInvoice() {
+    $(this).addClass("loading")
+    const schoolid = $(this).attr('schoolid')
+    $.ajax(`/admin/php/update?schoolid=${schoolid}&key=datepaid`, {
+        success: updateTeams, 
+        error: function() {
+            console.error("An error has occurred updating the school's invoice (233)");
+        }
+    })
+}
+
+function updateTeams() {
+    $.ajax("/admin/api/schools", {
+        success: function(data) {
+            var jsonData = JSON.parse(data)
+            if (jsonData.length == 0) {
+                error("No Results!")
+                return
+            }
+            $("#schools tbody").html("")
+            for (const school of jsonData) {
+                $("#schools tbody").append(makeSchoolRow(school))
+            }
+        },
+        error: function() {
+            error("An Error Has Occurred!")
+        }
+    })
+}
+
+function makeSchoolRow(school) {
+    const time = moment(school.rdate).fromNow()
+    const row = $("<tr>")
+    const a = $("<a/>").attr('href', "/admin/school?schoolid=" + school.schoolid).html(school.sname)
+
+    var amount_due = 0;
+    for(const team of school.teams) 
+        switch (team.division) {
+            case "eagle":
+                amount_due += 60;
+                break;
+            case "gold":
+                amount_due += 60;
+                break;
+            case "blue":
+                amount_due += 80;
+                break;
+        }
+
+    var paid = $("<a/>", {
+        class: "ui label"
+    })
+    if(school.paid) 
+        paid.addClass("green").html("Paid")
+    else
+        paid.addClass("red").html("Due")
+
+    row.append($("<td/>").html(a))
+    row.append($("<td/>").html(`$${amount_due}`))
+
+    var view_button = $("<a/>", {
+        target: "_blank",
+        href: `/admin/invoice?schoolid=${school.schoolid}`
+    }).html($("<button/>", {
+        class: "ui compact labeled icon button"
+    }).append($("<i/>", {
+        class: "eye icon"
+    }), "View"))
+
+    row.append($("<td/>").html(view_button))
+
+    var datesent;
+    if (school.datesent != null) {
+        datesent = $("<button/>", {
+            class: "ui compact blue button sentInvoice",
+            style: "width: 85%",
+            schoolid: school.schoolid
+        }).append("Sent")
+    } else { 
+        datesent = $("<button/>", {
+            class: "ui compact labeled icon blue basic button sentInvoice",
+            schoolid: school.schoolid
+        }).append($("<i/>", {
+            class: "envelope outline icon"
+        }),"Sent")
+    }
+
+    row.append($("<td/>").html(datesent))
+
+    var datepaid; 
+    if (school.datepaid != null) {
+        datepaid = $("<button/>", {
+            class: "ui compact green button paidInvoice",
+            style: "width: 85%",
+            schoolid: school.schoolid
+        }).append("Paid")
+    } else {
+        datepaid = $("<button/>", {
+            class: "ui compact labeled icon green basic button paidInvoice",
+            schoolid: school.schoolid
+        }).append($("<i/>", {
+            class: "dollar sign icon"
+        }), "Paid")
+    }
+
+    row.append($("<td/>").html(datepaid))
+    row.append($("<td/>").html(time))
+
+    return row
+}
+</script>
---
+++ admin/php/update.php
@@ -0,0 +1,33 @@
+<?php
+require_once $_SERVER['DOCUMENT_ROOT'] . "/include/connection.php";
+
+if (!loggedin()) {
+    echo json_encode(array("status" => 401, "message" => "You are not authorized to access this page."));
+    exit();
+}
+
+if (!isset($_GET['schoolid'])) {
+    echo json_encode(array("status" => 400, "message" => "Bad Request."));
+    exit();
+}
+
+if (!isset($_GET['key'])) {
+    echo json_encode(array("status" => 400, "message" => "Bad Request."));
+    exit();
+}
+
+$schoolid = $_GET['schoolid'];
+$key = $_GET['key'];
+    
+$sql = "UPDATE invoice ";
+if ($key == "datesent")
+    $sql .= "SET datesent=NOW() ";
+else if ($key == "datepaid")
+    $sql .= "SET datepaid=NOW() ";
+
+$sql .= "WHERE schoolid=$schoolid";
+
+$res = $mysql->query($sql);
+
+echo json_encode(array("status" => 200, "message" => "Success"));
+?>
