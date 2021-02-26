31c540805bc954ca5e8900ff89066daba652f278
Jack Pfeiffer <jl5her@yahoo.com>
2021-02-26 15:53:12 -0600

    Allow CORS for registered_teams

--- api/registered_teams.php
+++ api/registered_teams.php
@@ -14,4 +14,5 @@ $rows = array();
 while ($row = $res->fetch_assoc()) $rows[] = $row;
 
 header("Content-type: application/json");
+header("Access-Control-Allow-Origin: *");
 echo json_encode($rows, JSON_NUMERIC_CHECK);
