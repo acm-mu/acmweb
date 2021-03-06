3a945d14e925e9f6e525e13b1e6baee7fe0573fe
Jack Pfeiffer <jl5her@yahoo.com>
2021-03-06 10:59:32 -0600

    Check if target.matches before using

--- js/global.js
+++ js/global.js
@@ -1,68 +1,68 @@
 function onEvent(eventName, elementSelector, handler) {
-  document.addEventListener(eventName, (e) => {
-    // loop parent nodes from the target to the delegation node
-    for (let target = e.target; target && target !== this; target = target.parentNode) {
-      if (target.matches(elementSelector)) {
-        handler.call(target, e);
-        break;
-      }
-    }
-  }, false);
+    document.addEventListener(eventName, (e) => {
+        // loop parent nodes from the target to the delegation node
+        for (let target = e.target; target && target !== this; target = target.parentNode) {
+            if (target.matches && target.matches(elementSelector)) {
+                handler.call(target, e);
+                break;
+            }
+        }
+    }, false);
 }
 
 function getUrlParameter(lookupKey) {
-  // Get the query portion of the URL, and cut off the '?'
-  const query = window.location.search.substring(1);
-  // Split them up separated by '&'
-  const variables = query.split('&');
+    // Get the query portion of the URL, and cut off the '?'
+    const query = window.location.search.substring(1);
+    // Split them up separated by '&'
+    const variables = query.split('&');
 
-  for (let i = 0; i < variables.length; i += 1) {
-    const variable = variables[i].split('=');
+    for (let i = 0; i < variables.length; i += 1) {
+        const variable = variables[i].split('=');
 
-    if (variable[0] === lookupKey) {
-      // If key does not have value, return TRUE that it exists, otherwise return its value.
-      return variable[1] === undefined ? true : decodeURIComponent(variable[1]);
+        if (variable[0] === lookupKey) {
+            // If key does not have value, return TRUE that it exists, otherwise return its value.
+            return variable[1] === undefined ? true : decodeURIComponent(variable[1]);
+        }
     }
-  }
-  return undefined;
+    return undefined;
 }
 
 const capitalize = (str) => ((typeof str === 'string') ? str.replace(/^\w/, (c) => c.toUpperCase()) : '');
 
 function highlightNavItem() {
-  // TODO: Maybe if nav item doesn't exist for page,
-  // look to add it before highlighting index. This would deprecate sidebar.js nav script
-  document.addEventListener('DOMContentLoaded', () => {
-    const navItems = document.querySelectorAll('.nav_item');
-    navItems.forEach((element) => {
-      if (window.location.href.indexOf(element.id) > 0) {
-        element.classList.add('active');
-      }
-    });
+    // TODO: Maybe if nav item doesn't exist for page,
+    // look to add it before highlighting index. This would deprecate sidebar.js nav script
+    document.addEventListener('DOMContentLoaded', () => {
+        const navItems = document.querySelectorAll('.nav_item');
+        navItems.forEach((element) => {
+            if (window.location.href.indexOf(element.id) > 0) {
+                element.classList.add('active');
+            }
+        });
 
-    // TODO: If no other nav item is highlighted, default to index
-  });
+        // TODO: If no other nav item is highlighted, default to index
+    });
 }
 
 function setTitle(prefix) {
-  highlightNavItem();
-  document.addEventListener('DOMContentLoaded', () => {
-    const titleElement = document.querySelector('h1.title');
-    let title;
-    let page;
-    if (titleElement) {
-      title = titleElement.innerText;
-      page = titleElement.getAttribute('page');
+    highlightNavItem();
+    document.addEventListener('DOMContentLoaded', () => {
+        const titleElement = document.querySelector('h1.title');
+        let title;
+        let page;
+        if (titleElement) {
+            title = titleElement.innerText;
+            page = titleElement.getAttribute('page');
 
-      if (page && page !== 'index') title = `${prefix} - ${page.charAt(0).toUpperCase() + page.slice(1)}`;
-    }
+            if (page && page !== 'index') title = `${prefix} - ${page.charAt(0).toUpperCase() + page.slice(1)}`;
+        }
 
-    document.title = prefix + (page && page !== 'index' ? ` - ${page.charAt(0).toUpperCase() + page.slice(1)}` : '');
-  });
+        document.title = prefix + (page && page !== 'index' ? ` - ${page.charAt(0).toUpperCase() + page.slice(1)}` : '');
+    });
 }
 
 function createElement(html) {
-  const temp = document.createElement('template');
-  temp.innerHTML = html.trim();
-  return temp.content.firstChild;
+    const temp = document.createElement('template');
+    temp.innerHTML = html.trim();
+    return temp.content.firstChild;
 }
