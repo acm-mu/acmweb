ee6ee951a284c07a529dea6f0666c53f02f43e47
Brad Cooley <brad.cooley@marquette.edu>
2021-03-13 18:13:02 -0500

    removed popup signup form

--- include/header.php
+++ include/header.php
@@ -18,8 +18,6 @@
 
         setTitle('ACM@MU');
     </script>
-    <script id="mcjs">
-    !function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/9fa8d85050948a789c7b95129/acbbc259e3c5d62fc521f5360.js");</script>
 </head>
 
 <body>
