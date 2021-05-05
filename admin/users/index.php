4632bf150a6b6b9ca681061d54191a801933e600
Alex Gebhard <39505376+a1g3@users.noreply.github.com>
2021-05-04 23:30:51 -0500

    Add function for delete user

--- admin/users/index.php
+++ admin/users/index.php
@@ -5,13 +5,14 @@
 
 <br /><br /><br />
 
-<table class="ui table">
+<table id="user-table" class="ui table">
 <thead>
   <tr> <th>User ID</th> <th>Username</th> <th>Full Name</th> <th>Last Login</th> <th>Role</th> <th></th></tr>
 </thead>
+<tbody>
 <?php
     $mysql->query("USE muhostin_acm");
-    $sql = "SELECT * FROM users ORDER BY uid DESC";
+    $sql = "SELECT * FROM users ORDER BY uid";
     $res = $mysql->query($sql);
 
     while($row = $res->fetch_assoc()) {
@@ -30,12 +31,22 @@
 
     echo '<tr> <td colspan=5 style="text-align: center"> <a href="">Create New User</a></td> </tr>';
 ?>
+<tbody />
 </table>
 <script>
     function deleteUser(uid){
         fetch('/admin/users/delete', {
             method: 'DELETE',
             body: uid
+        }).then(resp => {
+            if(resp.ok) {
+                location.reload();
+            } else {
+                alert("The user was not deleted successfully!");
+            }
         });
+    }
+    function addUser(){
+        document.querySelector('#user-table tbody').appendChild(makeTeamRow(team))
     }
 </script>
